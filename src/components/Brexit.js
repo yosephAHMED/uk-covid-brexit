6f27088d32b3a48ff52ea5b85a190758d8715184
YosephAhmed <yosephahmed@aol.com>
2020-12-14 18:52:36 -0500

    feat: stylish transition into Brexit page

--- src/components/Brexit.js
+++ src/components/Brexit.js
@@ -1,12 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import "../assets/Brexit.css";
 
-export default class Brexit extends Component {
-    render() {
-        return (
-            <div>
-                Hello from Brexit
-            </div>
-        )
+const styles = {
+    transition: "all 1s ease-in",
+  };
+  
+  export default class Brexit extends Component {
+    constructor() {
+      super();
+      this.state = {
+        // initial opacity and scale of title
+        opacity: 0,
+        scale: 10,
+        color: '#61dafb',
+      };
     }
-}
-
+  
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({
+          // set opacity and scale to normal
+          opacity: 1,
+          scale: 1,
+          color: 'white',
+        });
+      }, 500);
+    }
+  
+    render() {
+      return (
+        <div className="covid-container">
+          <h1
+            className="covid-title"
+            style={{
+              ...styles,
+              opacity: this.state.opacity,
+              transform: "scale(" + this.state.scale + ")",
+              color: this.state.color,
+              animationName: 'changeColor',
+              animationDelay: '1s',
+              animationDuration: '5s',
+            }}
+          >
+            BREXIT
+          </h1>
+        </div>
+      );
+    }
+  }
