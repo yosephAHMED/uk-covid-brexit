7bb4f390b44551716b57537c1637aad29fd2c8af
YosephAhmed <yosephahmed@aol.com>
2020-12-14 23:57:42 -0500

    feat: finished home page for Covid component. Display case and death information to screen in a card

--- src/components/Covid.js
+++ src/components/Covid.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import axios from "axios";
 import "../assets/Covid.css";
 import Navbar from "./CovidComponents/Navbar";
+import CardImageOne from "../assets/images/2_LU.jpg";
+import CardImageTwo from "../assets/images/corona.jpg";
 
 const styles = {
   transition: "all 1s ease-in",
@@ -38,30 +40,68 @@ export default class Covid extends Component {
       });
   }
 
-  componentDidUpdate() {
-    console.log("data: ", this.state.data[0]);
-    console.log("data: ", this.state.data[1]);
-  }
-
   render() {
-    return (
-      <div className="covid-container">
-        <Navbar />
-        <h1
-          className="covid-title"
-          style={{
-            ...styles,
-            opacity: this.state.opacity,
-            transform: "scale(" + this.state.scale + ")",
-            color: this.state.color,
-            animationName: "changeColor",
-            animationDelay: "1s",
-            animationDuration: "5s",
-          }}
-        >
-          UK: The Covid Crisis
-        </h1>
-      </div>
-    );
+    if (this.state.data[0] === undefined) {
+      return <div>Fetching data from API...</div>;
+    } else {
+      return (
+        <div className="covid-container">
+          <Navbar />
+          <h1
+            className="covid-title"
+            style={{
+              ...styles,
+              opacity: this.state.opacity,
+              transform: "scale(" + this.state.scale + ")",
+              color: this.state.color,
+              animationName: "changeColor",
+              animationDelay: "1s",
+              animationDuration: "5s",
+            }}
+          >
+            UK: The Covid Crisis
+          </h1>
+          <div className="card">
+            <h2>London Reveals New Transport Rules</h2>
+            <a
+              href="https://www.mylondon.news/news/zone-1-news/transport-london-reveals-new-rules-18362065"
+              title="Transport for London reveals new rules about travel for over 60s using London Underground and buses"
+            >
+              <img src={CardImageOne} alt="logo" />
+            </a>
+            <div className="container">
+              <h1>
+                <b>{this.state.data[0].date}</b>
+              </h1>
+              <p>Area: {this.state.data[0].areaName}</p>
+              <p>Today New Cases: {this.state.data[0].newCasesByPublishDate}</p>
+              <p>Today New Deaths: NYD</p>
+              <p>Total Cases: {this.state.data[0].cumCasesByPublishDate}</p>
+              <p>Total Deaths: NYD</p>
+            </div>
+          </div>
+          <div className="card">
+            <h2>'New variant' of Coronavirus Identified in England</h2>
+            <a
+              href="https://www.bbc.com/news/health-55308211"
+              title="A new variant of coronavirus has been found which is growing faster in some parts of England, MPs have been told"
+            >
+              <img src={CardImageTwo} alt="logo" width="615" height="409" />
+            </a>
+            <div className="container">
+              <h1>
+                <b>{this.state.data[1].date}</b>
+              </h1>
+              <p>Area: {this.state.data[1].areaName}</p>
+              <p>Today New Cases: {this.state.data[1].newCasesByPublishDate}</p>
+              <p>Today New Deaths: {this.state.data[1].newDeathsByDeathDate}</p>
+              <p>Total Cases: {this.state.data[1].cumCasesByPublishDate}</p>
+              <p>Total Deaths: {this.state.data[1].cumDeathsByDeathDate}</p>
+            </div>
+          </div>
+          <h4>https://github.com/publichealthengland/coronavirus-dashboard-api-python-sdk</h4>
+        </div>
+      );
+    }
   }
 }
